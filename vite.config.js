b0b0b7f6adc147111613113fb79555e5403ea599
george <ezegeorgechukwuma@gmail.com>
2025-04-23 15:42:41 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/myPlantShop",
-  plugins: [react()],
+  base: '/myPlantShop/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html',
+          dest: '', // root of dist
+          rename: '404.html' // copy index.html as 404.html
+        }
+      ]
+    })
+  ]
 })
